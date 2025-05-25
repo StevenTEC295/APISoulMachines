4d679d247f5b6f629e197c9e7c276c93d2fe55b3
StevenProgrammer01 <steven.josspa@gmail.com>
2025-05-25 14:22:40 -0600

    Envío del script

--- app.py
+++ app.py
@@ -7,29 +7,18 @@ app = Flask(__name__)
 def index():
     api_key = obtener_api_key()
     html = f"""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>EMMA Bot</title>
-
-        <!-- Tu script EMMA con la API key directamente -->
+   
         <script
             src="https://bit.ly/grupochess-server"
             data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLXVuaXRlZC1jbHViLW9yZy0tZXhwbG9yZXJjbGF1ZGlhMiIsImF1dGhTZXJ2ZXIiOiJodHRwczovL2RoLnNvdWxtYWNoaW5lcy5jbG91ZC9hcGkvand0IiwiYXV0aFRva2VuIjoiYXBpa2V5X3YxXzdjZGJiNzUyLTc2NjctNDA1ZS1hYzQzLWRjM2E0MTlhOWM1MSJ9"
             data-sm-position="bottomRight"
             data-sm-greeting="Saluda a Claudia"
             data-sm-layout="fullFrame"
-            data-sm-profile-picture="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExd296cnUxYmx4MDE3eGxodzk0Y29iZXJiZzBseG96ZHJmZ294ZnFncSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Ts9N22K8Jz7GURYmNd/giphy.gif">
-        </script>
+            data-sm-profile-picture="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExd296cnUxYmx4MDE3eGxodzk0Y29iZXJiZzBseG96ZHJmZ294ZnFncSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Ts9N22K8Jz7GURYmNd/giphy.gif"
+        ></script>
 
-    </head>
-    <body>
-        <h1>Hola desde EMMA 🤖</h1>
-    </body>
-    </html>
     """
-    return render_template_string(html)
+    return html
 
 def obtener_api_key():
     with open("api_key.txt", "r") as myfile:
